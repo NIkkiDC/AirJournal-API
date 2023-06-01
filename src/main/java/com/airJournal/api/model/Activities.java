539c1a335fe1369a987228aef30df295a5384b7d
Dominique <DCWTLT1@gmail.com>
2023-06-01 12:36:02 -0500

    updating ids

--- src/main/java/com/airJournal/api/model/Activities.java
+++ src/main/java/com/airJournal/api/model/Activities.java
@@ -1,4 +1,18 @@
 package com.airJournal.api.model;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table
 public class Activities {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private String description;
+
 }
