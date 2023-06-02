2172456f870c8276dbecfd787a8959514e2a216b
Dominique <DCWTLT1@gmail.com>
2023-06-02 15:27:12 -0500

    getters and setters

--- src/main/java/com/airJournal/api/model/City.java
+++ src/main/java/com/airJournal/api/model/City.java
@@ -19,5 +19,19 @@ public class City {
     @JsonIgnore
     @ManyToOne // many cities to one activity
     @JoinColumn(name = "activities_id")
+    private Activities activities;
 
+    public City() {}
+
+    public Long getId() {return id;}
+
+    public void setId(Long id) {this.id = id;}
+
+    public String getName() {return name;}
+
+    public void setName(String name) {this.name = name;}
+
+    public String getDescription() {return description;}
+
+    public void setDescription(String description) {this.description = description;}
 }
