b6e88594aea443b03822ff1972def4041af31a3d
Dominique <DCWTLT1@gmail.com>
2023-06-02 16:47:52 -0500

    2 updates

--- src/main/java/com/airJournal/api/seed/ActivityData.java
+++ src/main/java/com/airJournal/api/seed/ActivityData.java
@@ -10,28 +10,28 @@ import org.springframework.stereotype.Component;
  * This method is called automatically when the application starts, and it allows you to execute custom logic or tasks.
  */
 
-@Component
-public class ActivityData implements CommandLineRunner {
+//@Component
+//public class ActivityData implements CommandLineRunner {
 
     /**
      * 21 The @Autowired automatic dependency injection
      * 22 retrieve activity data, save new activities, update existing activities, or delete activities.
      */
 
-    @Autowired
-    ActivityRepo activityRepo;
+//    @Autowired
+//    ActivityRepo activityRepo;
 
     /**
      * @param args the method will accept a variable number of arguments
      * @throws Exception
      */
 
-    @Override // annotation is used to indicate that the method is meant to override a method in a superclass or implement an interface method.
-    public void run(String... args) throws Exception {
-        dataForActivity();
-    }
-    private void dataForActivity () {
-        if (activityRepo)
-    }
+//    @Override // annotation is used to indicate that the method is meant to override a method in a superclass or implement an interface method.
+//    public void run(String... args) throws Exception {
+//        dataForActivity();
+//    }
+//    private void dataForActivity () {
+//        if (activityRepo)
+//    }
 
-}
+//}
