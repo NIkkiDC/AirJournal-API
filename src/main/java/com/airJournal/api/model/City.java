b1194aa32a2d372caacb3729f4d8933018c165be
Dominique <DCWTLT1@gmail.com>
2023-06-02 12:33:28 -0500

    creating columns

--- src/main/java/com/airJournal/api/model/City.java
+++ src/main/java/com/airJournal/api/model/City.java
@@ -1,9 +1,16 @@
 package com.airJournal.api.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="city")
 public class City {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private String description;
 }
