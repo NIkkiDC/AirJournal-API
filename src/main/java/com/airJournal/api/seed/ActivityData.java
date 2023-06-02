f3d56852c5a8937a2d3b604a0c8c2de183a3760d
Dominique <DCWTLT1@gmail.com>
2023-06-02 16:25:11 -0500

    creating param tht throws an exception

--- src/main/java/com/airJournal/api/seed/ActivityData.java
+++ src/main/java/com/airJournal/api/seed/ActivityData.java
@@ -1,4 +1,18 @@
 package com.airJournal.api.seed;
 
-public class ActivityData {
+import org.springframework.boot.CommandLineRunner;
+
+public class ActivityData implements CommandLineRunner {
+
+    /**
+     *
+     * @param args the method will accept a variable number of arguments
+     * @throws Exception
+     */
+
+    @Override // annotation is used to indicate that the method is meant to override a method in a superclass or implement an interface method.
+    public void run(String... args) throws Exception {
+        dataForActivity();
+    }
+
 }
