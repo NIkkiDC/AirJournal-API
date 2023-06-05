4cb26472ce37b303efe2d80f616d4c0a45372df0
Dominique <DCWTLT1@gmail.com>
2023-06-05 12:47:28 -0500

    controller for the city

--- src/main/java/com/airJournal/api/seed/CityData.java
+++ src/main/java/com/airJournal/api/seed/CityData.java
@@ -1,4 +1,9 @@
 package com.airJournal.api.seed;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api") // http://localhost:90
 public class CityData {
 }
