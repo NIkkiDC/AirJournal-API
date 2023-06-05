035d02343b0477646299eea1b9d1673e01b414cc
Dominique <DCWTLT1@gmail.com>
2023-06-05 13:24:28 -0500

    test endpoint

--- src/main/java/com/airJournal/api/controller/CityController.java
+++ src/main/java/com/airJournal/api/controller/CityController.java
@@ -1,5 +1,8 @@
 package com.airJournal.api.controller;
 
+import com.airJournal.api.repository.CityRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class CityController {
 
+    private CityRepo cityRepo;
+
+    @Autowired
+    public void setCityRepo(CityRepo cityRepo) {this.cityRepo = cityRepo; }
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {return "helloWorld";}
 }
