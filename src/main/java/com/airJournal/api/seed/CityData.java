7a5b64e88835f5481a415568d7fea1eed18bf8da
Dominique <DCWTLT1@gmail.com>
2023-06-05 12:03:05 -0500

    Grand Baie city 4

--- src/main/java/com/airJournal/api/seed/CityData.java
+++ src/main/java/com/airJournal/api/seed/CityData.java
@@ -20,6 +20,8 @@ public class CityData implements CommandLineRunner {
             City city1 = new City(1L, "Nairobi", "Nairobi is the capital and largest city of Kenya. The name is derived from the Maasai phrase Enkare Nairobi.");
             City city2 = new City(2L, "Mombasa", "Mombasa is a coastal city in southeastern Kenya along the Indian Ocean. It was the first capital of British East Africa." );
             City city3 = new City(3L, "Masai Mara", "Maasai Mara National Reserve is an area of preserved savannah wilderness in southwestern Kenya, along the Tanzanian border.");
+            City city4 = new City(4L, "Grand Baie", "Grand-Baie is a village in northern Mauritius. Its sandy beaches include secluded La Cuvette, and sailboat-dotted Grand-Baie Public Beach." );
+
 
 
 
