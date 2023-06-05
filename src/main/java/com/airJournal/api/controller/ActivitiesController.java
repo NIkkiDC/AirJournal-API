c90bba04bd6a5a9a97081c2608e416ae66f5264b
Dominique <DCWTLT1@gmail.com>
2023-06-05 13:03:48 -0500

    test commit

--- src/main/java/com/airJournal/api/controller/ActivitiesController.java
+++ src/main/java/com/airJournal/api/controller/ActivitiesController.java
@@ -1,4 +1,16 @@
 package com.airJournal.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//book controller
+@RestController
+@RequestMapping("/api")
 public class ActivitiesController {
+
+
+
+
+
 }
--- src/main/java/com/airJournal/api/controller/CityController.java
+++ src/main/java/com/airJournal/api/controller/CityController.java
@@ -1,4 +1,11 @@
 package com.airJournal.api.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//city list //author
+@RestController
+@RequestMapping
 public class CityController {
+
 }
