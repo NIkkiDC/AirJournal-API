cc400841d6705ea1b98f90dddb52491015c01e78
Dominique <DCWTLT1@gmail.com>
2023-06-06 11:58:44 -0500

    inputting all of the data to properly run test

--- src/test/java/com/airJournal/api/controller/CityControllerTest.java
+++ src/test/java/com/airJournal/api/controller/CityControllerTest.java
@@ -1,6 +1,8 @@
 package com.airJournal.api.controller;
 
+import com.airJournal.api.model.City;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -20,7 +22,9 @@ public class CityControllerTest {
 //    @MockBean
 //    private CityService;
 
-
+    City city1 = new City(1L, "Nairobi", "Nairobi is the capital and largest city of Kenya. The name is derived from the Maasai phrase Enkare Nairobi.");
+    City city2 = new City(2L, "Mombasa", "Mombasa is a coastal city in southeastern Kenya along the Indian Ocean. It was the first capital of British East Africa." );
+    City city3 = new City(3L, "Masai Mara", "Maasai Mara National Reserve is an area of preserved savannah wilderness in southwestern Kenya, along the Tanzanian border.");
 
 }
 
