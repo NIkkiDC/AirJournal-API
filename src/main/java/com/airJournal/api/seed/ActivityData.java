605502b3970f5fb4b41141d5e8339f135d31e834
Dominique <DCWTLT1@gmail.com>
2023-06-05 21:37:16 -0500

    set city 5

--- src/main/java/com/airJournal/api/seed/ActivityData.java
+++ src/main/java/com/airJournal/api/seed/ActivityData.java
@@ -56,6 +56,7 @@ public class ActivityData implements CommandLineRunner {
             activity2.setCity(city2);
             activity3.setCity(city3);
             activity4.setCity(city4);
+            activity5.setCity(city5);
             cityRepo.save(city1);
             cityRepo.save(city2);
             cityRepo.save(city3);
