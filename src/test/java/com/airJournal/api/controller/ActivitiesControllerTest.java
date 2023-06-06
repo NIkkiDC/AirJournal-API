53a4e31625205506129069e5625d8a68c3415fad
Dominique <DCWTLT1@gmail.com>
2023-06-06 16:17:45 -0500

    completed the final test, it has passed

--- src/test/java/com/airJournal/api/controller/ActivitiesControllerTest.java
+++ src/test/java/com/airJournal/api/controller/ActivitiesControllerTest.java
@@ -3,7 +3,6 @@ package com.airJournal.api.controller;
 import com.airJournal.api.model.Activity;
 import com.airJournal.api.repository.ActivityRepo;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -13,7 +12,11 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
 import static org.hamcrest.Matchers.notNullValue;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -34,12 +37,23 @@ public class ActivitiesControllerTest {
     public void getAllActivities_success() throws Exception {
         List<Activity> activities = new ArrayList<>(Arrays.asList(activity1,activity2,activity3));
 
-        Mockito.when(activityRepo.findAll()).thenReturn(activities); // testing activity repo
+        when(activityRepo.findAll()).thenReturn(activities); // testing activity repo
         mockMvc.perform(MockMvcRequestBuilders // this is testing the end point
                 .get("/api/activities/cityId")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$",notNullValue()))
+                .andDo(print());
+    }
+
+    @Test
+    public void getActivityById_success() throws Exception {
+        when(activityRepo.findById(anyLong())).thenReturn(Optional.ofNullable(activity1));
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/api/activities/{activitiesId}", 1L)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", notNullValue()))
                 .andDo(print());
     }
 
