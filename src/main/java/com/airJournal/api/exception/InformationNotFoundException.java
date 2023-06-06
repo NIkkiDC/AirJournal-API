f1afe9ac2502ea9fab2e6c25fb3dcf6d8deb40ed
Dominique <DCWTLT1@gmail.com>
2023-06-05 21:19:53 -0500

    creating Info NOT found exception

--- src/main/java/com/airJournal/api/exception/InformationNotFoundException.java
+++ src/main/java/com/airJournal/api/exception/InformationNotFoundException.java
@@ -1,4 +1,11 @@
 package com.airJournal.api.exception;
 
-public class InformationNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class InformationNotFoundException extends RuntimeException {
+    public InformationNotFoundException(String message) {
+        super(message);
+    }
 }
