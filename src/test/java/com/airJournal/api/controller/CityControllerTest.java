64296cbf8a5c09a04d1b3eaff4a3095874c2a962
Dominique <DCWTLT1@gmail.com>
2023-06-06 11:54:29 -0500

    updating file for test

--- src/test/java/com/airJournal/api/controller/CityControllerTest.java
+++ src/test/java/com/airJournal/api/controller/CityControllerTest.java
@@ -1,11 +1,25 @@
 package com.airJournal.api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @WebMvcTest(CityController.class)
 public class CityControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper mapper;
+//
+//    @MockBean
+//    private CityService;
+
 
 
 }
