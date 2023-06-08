c016edadb54d1231a28726c0abb8ef5704ee10c6
Dominique <DCWTLT1@gmail.com>
2023-06-08 14:34:19 -0500

    updates

--- src/main/java/com/airJournal/api/seed/ActivityData.java
+++ src/main/java/com/airJournal/api/seed/ActivityData.java
@@ -41,17 +41,17 @@ public class ActivityData implements CommandLineRunner {
     }
     private void dataForActivity () {
         if (activityRepo.count() == 0 || cityRepo.count() == 0) {
-            City city1 = new City(1L, "Mombasa", "Mombasa is a coastal city in southeastern Kenya along the Indian Ocean. It was the first capital of British East Africa." );
-            City city2 = new City(2L, "Masai Mara", "Maasai Mara National Reserve is an area of preserved savannah wilderness in southwestern Kenya, along the Tanzanian border.");
-            City city3 = new City(3L, "Grand Baie", "Grand-Baie is a village in northern Mauritius. Its sandy beaches include secluded La Cuvette, and sailboat-dotted Grand-Baie Public Beach." );
-            City city4 = new City(4L, "Mahé", "Mahé is the largest island in the Seychelles archipelago, in the Indian Ocean off East Africa.The island is also home to Victoria, Seychelles' capital." );
-            City city5 = new City(5L, "La Digu", "La Digue is an island in the Seychelles, in the Indian Ocean off East Africa. It’s known for its beaches. ");
+            City city1 = new City(1L, "Mombasa, Kenya", "Mombasa, is a coastal city in southeastern Kenya along the Indian Ocean. It was the first capital of British East Africa." );
+            City city2 = new City(2L, "Masai Mara, Kenya", "Maasai Mara, National Reserve is an area of preserved savannah wilderness in southwestern Kenya, along the Tanzanian border.");
+            City city3 = new City(3L, "Grand Baie, Mauritius", "Grand-Baie, is a village in northern Mauritius. Its sandy beaches include secluded La Cuvette, and sailboat-dotted Grand-Baie Public Beach." );
+            City city4 = new City(4L, "Mahé, Seychelles", "Mahé, is the largest island in the Seychelles archipelago, in the Indian Ocean off East Africa.The island is also home to Victoria, Seychelles' capital." );
+            City city5 = new City(5L, "La Digu, Seychelles", "La Digue, is an island in the Seychelles, in the Indian Ocean off East Africa. It’s known for its beaches. ");
 
-            Activity activity1 = new Activity(1L,"Camel Riding", "Riding camels on the beach, beautiful views");
-            Activity activity2 = new Activity(2L,"Hot Air Balloon", "The pilot will apply heat to make the balloon slowly rise off the ground.");
-            Activity activity3 = new Activity(3L,"Parasailing","the recreational activity of gliding through the air wearing an open parachute while being towed by a motorboat.");
+            Activity activity1 = new Activity(1L,"Camel Riding", "Riding camels on the beach, with beautiful views");
+            Activity activity2 = new Activity(2L,"Hot Air Balloon", "The pilot will apply heat to make the balloon slowly rises off the ground.");
+            Activity activity3 = new Activity(3L,"Parasailing","The recreational activity of gliding through the air wearing an open parachute while being towed by a motorboat.");
             Activity activity4 = new Activity(4L,"Helicopter ride","Explore Seychelles Islands from above on a helicopter tour organized by Adventure Life." );
-            Activity activity5 = new Activity(5L,"Clear Kayaking", "paddling a small craft through water, the boat is clear so that you can see down below you.");
+            Activity activity5 = new Activity(5L,"Clear Kayaking", "Paddling a small craft through water, the boat is clear so that you can see down below you.");
             activity1.setCity(city1);
             activity2.setCity(city2);
             activity3.setCity(city3);
